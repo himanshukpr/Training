f46b839538dadf205541b4e67d208d6960e29015
Your Name <you@example.com>
2025-06-18 11:16:51 +0530

    first commit

---
+++ Session 3/2_model.py
@@ -0,0 +1,12 @@
+a_age = 25
+b_age = a_age # reference Copy operation
+c_age = 25 # will make the new storage of the stack but will refere to the same heap HASHCODE 
+
+# if it delete both then it will be dynamic opinter
+del a_age # this will just delete the reference of the variable a_age from the stack but not from the  heap
+
+# If it will not delete from the heap then it will be a memory leak
+
+# print(a_age, id(a_age), type(a_age))
+print(b_age, id(b_age), type(b_age))
+print(c_age, id(c_age), type(c_age))
---
+++ Session 3/Model.py
@@ -0,0 +1,20 @@
+# model
+# single value
+# multi value
+
+
+# create statements
+instagram_user_name = "Himanshu" #SNAKE CASE
+# ldh : referenev varialbe (name of the storge container)
+# rhs : data
+
+followers = 300
+# ldh : referenev varialbe (name of the storge container)
+# rhs : data
+
+
+# read statements
+
+# it will store the name and the hashcode in the memory then it will refere to the HEAP where the actual data is stored
+print(instagram_user_name, id (instagram_user_name), type(instagram_user_name))
+print(followers, id(followers), type(followers))
