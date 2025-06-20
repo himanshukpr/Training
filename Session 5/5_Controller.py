b13776a387b02e876e6345492e4a0f706db91e51
Your Name <you@example.com>
2025-06-20 12:11:57 +0530

    controller

---
+++ Session 5/5_Controller.py
@@ -0,0 +1,27 @@
+# cab_fare = 110
+# tax = 0.18
+
+# tax_to_pay = cab_fare * tax
+# amount_to_pay = cab_fare + tax_to_pay
+
+# print(tax_to_pay)
+# print(amount_to_pay)
+
+# number1 = 10
+
+# print(number1)
+# print(hex(number1))
+# print(oct(number1))
+# print(bin(number1))
+
+
+# location = True
+# gps_location = False
+
+# google_map_location = location ^ gps_location
+
+# print(google_map_location)
+
+
+# bitwise
+
